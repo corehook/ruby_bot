a87bf7e678bd6bfb40616bd40607348c71da3cc0
root <root@localhost.localdomain>
2017-12-12 09:07:24 -0500

    eye gem

--- bot.rb
+++ bot.rb
@@ -2,7 +2,7 @@ require 'telegram/bot'
 require 'rubygems'
 require 'yaml'
 require 'erb'
-
+require 'eye'
 id = false
 token = "499649310:AAGhXHTXaGDXzgRBETofl1spj_tLMdnpfYU"
 Telegram::Bot::Client.run(token) do |bot |
@@ -13,14 +13,11 @@ Telegram::Bot::Client.run(token) do |bot |
 				Telegram::Bot::Types::KeyboardButton.new(text: '!list')
 			]
 			markup = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: kb)
-
 			bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}!, if you want to see list of files run : !list, or use !deploy filename", reply_markup: markup)
 		end
-
-		if message.from.username.index("cfcayana") == 0
+		if message.from.username == "cfcayan"
 			id = true
 		end	
-
 		if id == true
 			case message.text
 			when '!list' 
@@ -28,7 +25,7 @@ Telegram::Bot::Client.run(token) do |bot |
 					bot.api.send_message(chat_id: message.chat.id, text: file, reply_markup: markup)
 				}
 			end
-			if message.text.index("!deploy") == 0 && id ==true
+			if message.text.index("!deploy") == 0
 				a = message.text
 				a.slice!"!deploy "
 				Dir.glob(a) { | file |
@@ -37,7 +34,27 @@ Telegram::Bot::Client.run(token) do |bot |
 					bot.api.send_message(chat_id: message.chat.id, text: loaded_data.inspect, reply_markup: markup)
 				}
 			end
-		end
+			if message.text.index("!eye l") == 0
+				a = message.text 
+				a.slice!"!eye l "
+				Dir.glob(a) { | file |
+					bot.api.send_message(chat_id: message.chat.id, text: file, reply_markup: markup)
+					
+					output = system "eye l #{file}"
+result = $?.success?
+					if $?.exitstatus > 0
+						bot.api.send_message(chat_id: message.chat.id, text: "failed", reply_markup: markup)	
+					else
+bot.api.send_message(chat_id: message.chat.id, text: "output is #{output}", reply_markup: markup)	
+end}
+			end
+			if message.text.index("!eye i") == 0
+					a = message.text 
+					system "eye i"
+					if $?.exitstatus > 0
+						bot.api.send_message(chat_id: message.chat.id, text: "failed", reply_markup: markup)	
+					end
+				end
+			end					
+		end				
 	end
-end
-
