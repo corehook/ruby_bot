2820125cfb3a54c27e9147a5c4a4cfeff461bda7
corehook <corehook@users.noreply.github.com>
2017-12-13 19:35:53 +0600

    Create refactoring.rb

---
+++ refactoring.rb
@@ -0,0 +1,35 @@
+require 'telegram/bot'
+require 'rubygems'
+require 'pry'
+
+token = "499649310:AAGhXHTXaGDXzgRBETofl1spj_tLMdnpfYU"
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message |
+    case message.text
+
+      when '/start', 'Start', 'hi', 'hey', '/START', '/Start', 'hey', 'hi', 'Hi'
+        kb = [
+          Telegram::Bot::Types::KeyboardButton.new(text: '/list')
+        ]
+        markup = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: kb)
+
+        bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}!, if you want to see list of files run : /list, or use !deploy filename", reply_markup: markup)
+
+      when '!list', '/list'
+        Dir.glob("*.yml") { | file | bot.api.send_message(chat_id: message.chat.id, text: file, reply_markup: markup) }
+      else
+        if message.text.index("!deploy") == 0
+          a = message.text
+          a.slice!"!deploy "
+          Dir.glob(a) { | file |
+              bot.api.send_message(chat_id: message.chat.id, text: file, reply_markup: markup)
+            # loaded_data = YAML.load(ERB.new(File.read(file)).result)
+            result_log = `ansible-playbook #{file} -i hosts 2>&1`
+            bot.api.send_message(chat_id: message.chat.id, text: result_log, reply_markup: markup)
+          }
+        else
+          bot.api.send_message(chat_id: message.chat.id, text: "I didn't understand you:(, #{message.from.first_name} ")
+        end
+      end
+  end
+end
